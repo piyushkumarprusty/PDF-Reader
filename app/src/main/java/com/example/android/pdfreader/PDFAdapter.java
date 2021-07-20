d6e9a346c6a0c1786ff6cf8508c585c5cb56f88d
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-20 19:37:29 +0530

    get item const

--- app/src/main/java/com/example/android/pdfreader/PDFAdapter.java
+++ app/src/main/java/com/example/android/pdfreader/PDFAdapter.java
@@ -1,10 +1,13 @@
 package com.example.android.pdfreader;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -16,12 +19,25 @@ public class PDFAdapter extends ArrayAdapter<File> {
     ViewHolder viewHolder;
     ArrayList<File> al_pdf;
 
-    public PDFAdapter( Context context,  ViewHolder viewHolder, ArrayList<File> al_pdf) {
-        super(context, R.layout.adapter_pdf,al_pdf);
-        this.viewHolder = viewHolder;
+    public PDFAdapter(Context context, ArrayList<File> al_pdf) {
+        super(context, R.layout.adapter_pdf, al_pdf);
+        this.context = context;
         this.al_pdf = al_pdf;
     }
 
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        if(al_pdf.size()>0){
+            return  al_pdf.size();
+        }
+        else return 1;
+    }
 
     public class ViewHolder {
         TextView tv_filename;
