ecf873073b994cc9537c648a8bae08f2ac7a9cd9
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-20 19:24:14 +0530

    const rem

--- app/src/main/java/com/example/android/pdfreader/PDFAdapter.java
+++ app/src/main/java/com/example/android/pdfreader/PDFAdapter.java
@@ -13,50 +13,15 @@ import java.util.List;
 public class PDFAdapter extends ArrayAdapter<File> {
 
     Context context;
-ViewHolder viewHolder;
-ArrayList<File> al_pdf;
+    ViewHolder viewHolder;
+    ArrayList<File> al_pdf;
 
-    public PDFAdapter(@NonNull @org.jetbrains.annotations.NotNull Context context, int resource, Context context1, ViewHolder viewHolder, ArrayList<File> al_pdf) {
+    public PDFAdapter( Context context,  ViewHolder viewHolder, ArrayList<File> al_pdf) {
         super(context, resource);
-        this.context = context1;
         this.viewHolder = viewHolder;
         this.al_pdf = al_pdf;
     }
 
-    public PDFAdapter(@NonNull @org.jetbrains.annotations.NotNull Context context, int resource, int textViewResourceId, Context context1, ViewHolder viewHolder, ArrayList<File> al_pdf) {
-        super(context, resource, textViewResourceId);
-        this.context = context1;
-        this.viewHolder = viewHolder;
-        this.al_pdf = al_pdf;
-    }
-
-    public PDFAdapter(@NonNull @org.jetbrains.annotations.NotNull Context context, int resource, @NonNull @org.jetbrains.annotations.NotNull File[] objects, Context context1, ViewHolder viewHolder, ArrayList<File> al_pdf) {
-        super(context, resource, objects);
-        this.context = context1;
-        this.viewHolder = viewHolder;
-        this.al_pdf = al_pdf;
-    }
-
-    public PDFAdapter(@NonNull @org.jetbrains.annotations.NotNull Context context, int resource, int textViewResourceId, @NonNull @org.jetbrains.annotations.NotNull File[] objects, Context context1, ViewHolder viewHolder, ArrayList<File> al_pdf) {
-        super(context, resource, textViewResourceId, objects);
-        this.context = context1;
-        this.viewHolder = viewHolder;
-        this.al_pdf = al_pdf;
-    }
-
-    public PDFAdapter(@NonNull @org.jetbrains.annotations.NotNull Context context, int resource, @NonNull @org.jetbrains.annotations.NotNull List<File> objects, Context context1, ViewHolder viewHolder, ArrayList<File> al_pdf) {
-        super(context, resource, objects);
-        this.context = context1;
-        this.viewHolder = viewHolder;
-        this.al_pdf = al_pdf;
-    }
-
-    public PDFAdapter(@NonNull @org.jetbrains.annotations.NotNull Context context, int resource, int textViewResourceId, @NonNull @org.jetbrains.annotations.NotNull List<File> objects, Context context1, ViewHolder viewHolder, ArrayList<File> al_pdf) {
-        super(context, resource, textViewResourceId, objects);
-        this.context = context1;
-        this.viewHolder = viewHolder;
-        this.al_pdf = al_pdf;
-    }
 
     public class ViewHolder {
         TextView tv_filename;
